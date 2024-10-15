541316c8749f3bb1381193bab99060214a3ed111
Matt Stevenson <matt@bright-interactive.com>
2024-10-15 11:39:42 +0100

    ABC-8769 Add support for the blend-align and blend-alpha options to try and facilitate using SS for watermarks

--- src/image-ops/blending.js
+++ src/image-ops/blending.js
@@ -1,8 +1,11 @@
 const axios = require('axios')
 
 exports.apply = async (image, edits) => {
-  if (edits.blend.processedValue) {
-    await this.blend(image, edits.blend.processedValue)
+  await this.beforeApply(image, edits)
+  const { blend, blendalign, blendalpha } = edits
+
+  if (blend.processedValue) {
+    await this.blend(image, blend.processedValue, blendalign.processedValue, blendalpha.processedValue)
   }
 }
 
@@ -10,12 +13,68 @@ exports.apply = async (image, edits) => {
  *
  * @param {Sharp} image
  * @param {String} url
- */
-exports.blend = async (image, url) => {
+ * @param {String} gravity
+ * @param {number} alpha
+ **/
+exports.blend = async (image, url, gravity, alpha) => {
   const compositeInput = (await axios({
     url: url,
     responseType: "arraybuffer"
   })).data;
 
-  image.composite([{input: compositeInput, tile: true}])
+  let inputImage = {input: compositeInput, tile: false, gravity: gravity}
+
+  if (alpha >= 0) {
+    colour = {r: 10, g: 10, b: 10, a: alpha}
+    inputImage = {...inputImage, create: {background: colour}}
+  }
+
+  image.composite([inputImage])
+}
+
+exports.beforeApply = async function (image, edits) {
+  const { blendalign, blendalpha } = edits
+
+  const alignment = blendAlign.processedValue
+  if (alignment) {
+    switch (alignment) {
+      case 'center,middle' || 'middle,center':
+        blendalign.processedValue = 'centre'
+        break
+      case 'left,middle' || 'middle,left':
+        blendalign.processedValue = 'west'
+        break
+      case 'right,middle' || 'middle,right':
+        blendalign.processedValue = 'east'
+        break
+      case 'top,middle' || 'middle,top':
+        blendalign.processedValue = 'north'
+        break
+      case 'top,left' || 'left,top':
+        blendalign.processedValue = 'northwest'
+        break
+      case 'top,right' || 'right,top':
+        blendalign.processedValue = 'northeast'
+        break
+      case 'bottom,middle' || 'middle,bottom':
+        blendalign.processedValue = 'south'
+        break
+      case 'bottom,left' || 'left,bottom':
+        blendalign.processedValue = 'southwest'
+        break
+      case 'bottom,right' || 'right,bottom':
+        blendalign.processedValue = 'southeast'
+        break
+      default:
+        blendalign.processedValue = 'centre'
+        console.error('Invalid blending alignment: ' + alignment)
+    }
+
+    if (blendalpha.processedValue) {
+      const alphaAsDecimal = parseFloat(blendalpha.processedValue) / 100
+      blendalpha.processedValue = alphaAsDecimal
+    } else {
+      blendalpha.processedValue = -1
+    }
+  }
 }
